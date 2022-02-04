50df0151da7f75897b15e6ed666dc29e1a095b86
Lukasz Zytko <zytko.lukasz@gmail.com>
2022-02-04 01:41:29 +0100

    Change JSON to DB

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template, redirect, url_for
+
+from forms import TodoForm
+from sqlite_models import todos_sql
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "nininini"
+
+@app.route("/todos/", methods=["GET", "POST"])
+def todos_list():
+    form = TodoForm()
+    error = ""
+    if request.method == "POST":
+        if form.validate_on_submit():
+            todos_sql.create(form.data)
+        return redirect(url_for("todos_list"))
+
+    return render_template("todos.html", form=form, todos=todos_sql.all(), error=error)
+
+
+@app.route("/todos/<int:todo_id>/", methods=["GET", "POST"])
+def todo_details(todo_id):
+    todoql = todos_sql.get(todo_id)
+    form = TodoForm(data=todoql)
+    if request.method == "POST":
+        if form.validate_on_submit():
+            todos_sql.update(todo_id, form.data)
+        return redirect(url_for("todos_list"))
+    return render_template("todo.html", form=form, todo_id=todo_id)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ sqlite_models.py
@@ -0,0 +1,104 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection():
+    db_file = "database.db"
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)    
+    return conn
+
+
+class TodosSQLite:
+    def __init__(self):
+        try:
+            self.conn = create_connection()
+            self.cur = self.conn.cursor()
+            self.cur.execute("SELECT * FROM todos")
+            self.todos_sqlite = self.cur.fetchall()
+            self.conn.close()
+        except FileNotFoundError:
+            self.todos_sqlite = []
+
+    def all(self):
+        self.conn = create_connection()
+        self.cur = self.conn.cursor()
+        self.cur.execute(f"SELECT * FROM todos")
+        self.todos_sqlite = self.cur.fetchall()
+        self.conn.close()
+        return self.todos_sqlite
+    
+    def get(self, id):
+        self.conn = create_connection()
+        self.cur = self.conn.cursor()
+        self.cur.execute(f"SELECT * FROM todos WHERE id={id}")
+        self.row = self.cur.fetchone()
+        self.conn.close()
+        dict = {"id": self.row[0], "title": self.row[1], "description": self.row[2], "done": self.row[3]}
+        return dict
+    
+    def create(self, data):
+        data.pop('csrf_token')
+        self.conn = create_connection()
+        self.cur = self.conn.cursor()
+        sql = """INSERT INTO todos (title, description, done) VALUES (?,?,?)"""
+        values = []
+        for val in data.values():
+            values.append(val)
+        self.cur.execute(sql,values)
+        self.conn.commit()
+        self.conn.close()
+        return self.cur.lastrowid
+
+    def update(self, id, data):
+        data.pop('csrf_token')
+        self.parameters = [f"{k} = ?" for k in data.keys()]
+        self.parameters = ", ".join(self.parameters)
+        self.values = tuple(v for v in data.values())
+        self.values += (id, )
+
+        self.sql = f''' UPDATE todos
+                    SET {self.parameters}
+                    WHERE id = ?'''
+        self.conn = create_connection()            
+        try:
+            self.cur = self.conn.cursor()
+            self.cur.execute(self.sql, self.values)
+            self.conn.commit()
+        except sqlite3.OperationalError as e:
+            print(e)
+
+todos_sql = TodosSQLite()
+
+
+
+
+"""
+def create_table():
+    conn = None
+    db_file = "database.db"
+    try:
+        conn = sqlite3.connect(db_file)
+        cur = conn.cursor()
+        cur.execute('''CREATE TABLE IF NOT EXISTS todos (
+            id integer PRIMARY KEY, 
+            title VARCHAR(250) NOT NULL, 
+            description text NOT NULL,
+            done Boolean);''')
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+create_table()
+
+conn = create_connection()
+cur = conn.cursor()
+cur.execute('''INSERT INTO todos (title, description, done) VALUES ("test3","test test","True")''')
+conn.commit()
+conn.close()
+"""
